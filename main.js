5914b5e2695e457052d08d83e2f489cd49ee900d
usama280 <usama280@hotmail.com>
2021-01-29 12:17:16 -0600

    Brain

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ client.on('message', message => {
         message.channel.send('Why cc ofc');
     }
 
+    else if(message.content.slice(prefix.length) === 'PasteBot show me your brain'){
+        message.channel.send('https://myanimelist.net/animelist/PasteLord&view=tile&status=2');
+    }
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
@@ -31,4 +34,4 @@ client.on('message', message => {
 
 
 
-client.login('ODA0NzU2ODE2OTg2MTEyMDIx.YBQ-MA.Llgcx7LGTKgfd9FebUh4u8WAeOE');
+client.login(process.env.token);

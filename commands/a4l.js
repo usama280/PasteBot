0a209b1cdb8a476b9fa0fca187b1179ea53c91c9
usama280 <usama280@hotmail.com>
2021-01-29 19:08:09 -0600

    taco

--- commands/a4l.js
+++ commands/a4l.js
@@ -5,7 +5,7 @@ module.exports = {
         if((Math.floor(Math.random() * 10) + 1) > 8){
             message.channel.send('ATAGO FOR LIFE!!!');
         }else{
-            message.channel.send('You uncultered swine, cc >> atago');
+            message.channel.send('You uncultered swine, cc >> atago (taco)');
         }
     }
 }

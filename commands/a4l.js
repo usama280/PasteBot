aaa63ab3e892cb9e7073770e727293194e0c248b
usama280 <usama280@hotmail.com>
2021-01-29 19:06:59 -0600

    comm

---
+++ commands/a4l.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'a4l',
+    description: "best waifu",
+    execute(message, args){
+        if((Math.floor(Math.random() * 10) + 1) > 8){
+            message.channel.send('ATAGO FOR LIFE!!!');
+        }else{
+            message.channel.send('You uncultered swine, cc >> atago');
+        }
+    }
+}

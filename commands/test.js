c645bf6aee46f8c0072264fbc3006debae8fddd5
usama280 <usama280@hotmail.com>
2021-01-30 15:55:12 -0600

    cache

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "this is a test role",
     execute(message, args){
 
-        if(message.member.roles.cashe.has('805190796508790784')){
+        if(message.member.roles.cache.has('805190796508790784')){
             message.channel.send('You are a part of the test role!');
         }else{
             message.channel.send('You are not a part of the test role, *cough cough* loser.');

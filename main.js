3b2fe39c267de8bdbc4a44323553724abe50e9ab
usama280 <usama280@hotmail.com>
2021-01-29 18:57:19 -0600

    update

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
-const prefix = "-";
-
+ 
+const prefix = '-';
+ 
 const fs = require('fs');
-
+ 
 client.commands = new Discord.Collection();
-
+ 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
-
+ 
     client.commands.set(command.name, command);
 }
 

f66fff03c365859605f4541fded8541a4b51b5d5
usama280 <usama280@hotmail.com>
2021-01-31 12:02:07 -0600

    multi

--- main.js
+++ main.js
@@ -17,14 +17,23 @@ client.once('ready', () => {
     console.log('PasteBot is online!');
 });
 
-client.on('message', message => {
-    let authId; 
+client.on('message', message => { 
     
     if(message.content.toLowerCase().includes("who is the coolest")){
         message.channel.send('Why cc ofc');
     } else if(message.content.toLowerCase().includes('add me to the test role')){
         message.channel.send('Say the magic word :)');
-        authId = message.author.id;
+        
+        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+        console.log(collector)
+        collector.on('collect', message => {
+            if (message.content.toLowerCase().includes('please')) {
+                message.channel.send('You cheeky bastard, fine');
+                message.member.roles.add('805190796508790784').catch(console.error);
+            } else {
+                message.channel.send("WRONG! You actually thought bro...");
+            }
+        })
     } else if(authId == message.author.id && message.content.toLowerCase().includes('please') ){
             message.channel.send('You cheeky bastard, fine');
             message.member.roles.add('805190796508790784').catch(console.error);

eca0ffc7f43e76e8bec25b878e323f1ce9ad9506
usama280 <usama280@hotmail.com>
2021-02-02 15:50:35 -0600

    embed upd

--- commands/embed.js
+++ commands/embed.js
@@ -4,14 +4,14 @@ module.exports = {
     execute(message, args, Discord){
         const newEmb = new Discord.MessageEmbed()
         .setColor('#FFA300')
-        .setTitle('Test')
-        .setDescription('Test Descript.')
+        .setTitle('Hippo')
+        .setDescription('This is a Hippo')
         .addFields(
-            {name: 'Test 1', value: 'cc is cool'},
-            {name: 'Test 2', value: 'atago is cool, sike'}
+            {name: 'Fact 1', value: 'Hippos think cc is cool'},
+            {name: 'Fact 2', value: 'Hippos think atago is cool, sike!'}
         )
         .setImage('https://cdn.shopify.com/s/files/1/2281/5369/products/hippopotamus-838129_1000x.jpg?v=1580799007')
-        .setFooter('Test Footer');
+        .setFooter('Hippos like tacos');
             
         message.channel.send(newEmb);
     }
